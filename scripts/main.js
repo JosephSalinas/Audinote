f3d64ce22fe1a8563d8bfb455a0ab02e2eb47ad6
Suraj <bissuraj@gmail.com>
2021-09-26 14:58:44 -0400

    Added speech recognition. Need to clean up code

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,38 @@
-function test() {
-  alert("Hey!!!!");
+// new speech recognition object
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+
+// This runs when the speech recognition service starts
+recognition.onstart = function () {
+  console.log("We are listening. Speak!");
+};
+
+// This runs when the speech recognition service returns result
+recognition.onresult = function (event) {
+  var transcript = event.results[0][0].transcript;
+  var confidence = event.results[0][0].confidence;
+  handleResults(transcript, confidence);
+};
+
+// start recognition
+// recognition.start();
+
+startBtn = document.getElementById("startBtn");
+stopBtn = document.getElementById("stopBtn");
+speechDump = document.getElementById("speechDump");
+
+startBtn.addEventListener("click", () => {
+  recognition.start();
+});
+
+stopBtn.addEventListener("click", () => {
+  recognition.stop();
+});
+
+function handleResults(transcript, confidence) {
+  speechDump.innerHTML =
+    "You said: " + transcript + " \nConfidence: " + confidence;
 }
+
+let mainBtn = document.getElementById("record");
+let recording = false;
