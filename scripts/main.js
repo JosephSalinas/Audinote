f65d084689ba4f459dbef579f8375b31bd6fc95e
Suraj <bissuraj@gmail.com>
2021-10-24 12:50:51 -0400

    Created local storage functions 1

--- scripts/main.js
+++ scripts/main.js
@@ -91,6 +91,10 @@ function makeCard(title, body, date) {
   cardsLocal.push({ title, body, date });
 }
 
+function makeNote(title, body, createdAt) {
+  return { title, body, createdAt };
+}
+
 /* <div class="note">
   <p class="text">This is a test note to test the UI of this app</p>
   <p class="date">September 26th, 2021</p>
@@ -101,35 +105,80 @@ function makeCard(title, body, date) {
  * @param Note
  * @returns Element
  */
-function noteToElem({title, body, createdAt}) {
-  let div = document.createElement("div")
-  div.classList.add("note")
-  let bodyp = document.createElement("p")
-  bodyp.textContent = body
-  bodyp.classList.add("text")
-  let dateelem = document.createElement("p")
-  let formattedDate = Date(createdAt).toLocaleDateString()
-  dateelem.textContent = formattedDate
-  dateelem.classList.add("date")
-  div.append(bodyp, dateelem)
-  return div
+function noteToElem({ title, body, createdAt }) {
+  let div = document.createElement("div");
+  div.classList.add("note");
+  let bodyp = document.createElement("p");
+  bodyp.textContent = body;
+  bodyp.classList.add("text");
+  let dateelem = document.createElement("p");
+  let formattedDate = Date(createdAt).toLocaleDateString();
+  dateelem.textContent = formattedDate;
+  dateelem.classList.add("date");
+  div.append(bodyp, dateelem);
+  return div;
 }
 
-const notesElem = document.getElementById("notes")
+const notesElem = document.getElementById("notes");
 
 function initializeNotes() {
-  let ids = getNoteIds()
-  let notes = idsToNotes(ids)
-  let noteElems = notes.map(note => noteToElem(note))
-  addNotesToPage(noteElems)
+  let ids = getNoteIds();
+  let notes = idsToNotes(ids);
+  let noteElems = notes.map((note) => noteToElem(note));
+  addNotesToPage(noteElems);
 }
 
 /**
  * Inserts a list of note elements into the page
- * @param notes Element[] 
+ * @param notes Element[]
  */
 function addNotesToPage(notes) {
-  let frag = document.createDocumentFragment()
-  notes.forEach(note => frag.append(note))
-  notesElem.append(frag)
+  let frag = document.createDocumentFragment();
+  notes.forEach((note) => frag.append(note));
+  notesElem.append(frag);
+}
+
+// function updateManifest(new_id) {
+//   const manifest = localStorage.manifest
+//   let manifest_arr
+//   if (manifest) {
+//     manifest_arr = JSON.parse(manifest);
+//   } else {
+//     manifest_arr = [];
+//   }
+//   return manifest_arr.push(new_id)
+
+// }
+
+/**
+ *
+ * @param {Number} new_id
+ * @returns Void
+ * Mutates manifest on LS by adding the new id to it
+ */
+function updateManifest(new_id) {
+  const oldManifest = JSON.parse(localStorage.getItem("manifest")) ?? [];
+  localStorage.setItem("manifest", JSON.stringify([...oldManifest, new_id]));
+}
+
+/**
+ *
+ * @param {Number} id
+ * @param {Note Object} note
+ * @returns Void
+ *
+ * Adds note at specified ID to local storage
+ */
+function persistNote(id, note) {
+  localStorage.setItem(`note-${id}`, JSON.stringify(note));
+}
+
+function addNote(title, body) {
+  const createdAt = Date.now();
+  const note = makeNote(title, body, createdAt);
+  const ids = getNoteIds();
+  const last_id = ids[ids.length - 1];
+  const curr_id = last_id + 1;
+  updateManifest(curr_id);
+  persistNote(curr_id, note);
 }
